fc5a610ce50692f16ad4e6718b6b6ee14af65f70
Paul Rosero <paul_rosero@yahoo.com>
2019-02-25 02:40:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  sentence1 = sentence.split
+  sentence2 = []
+  sentence1.each do |sentence|
+    sentence2 << sentence.reverse
+  end
+  sentence2.join(" ")
+
+  sentence1 = sentence.split
+  sentence2 = []
+  sentence1.collect do |sentence|
+    sentence2 << sentence.reverse
+  end
+  sentence2.join(" ")
+end
