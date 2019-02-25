b451e97bbb4da925913dea21074947d8e725f436
Paul Rosero <paul_rosero@yahoo.com>
2019-02-25 02:11:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence1 = sentence.split
 
-  sentence2 = sentence1.each do |sentence|
+  sentence1.each do |sentence1|
   end
-  sentence1.reverse
+  .reverse
 end
